bdad16fa5a6a27580454ad4d3d4cdf7e0dbcb294
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-04-22 00:42:33 +0500

    Сверстан бургер-меню

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger, .header__nav').toggleClass('active');
+	$('.header__burger').click(function(event){
+			$('.header__burger').toggleClass('active');
+			$('.header__nav').toggleClass('active');
+			$('.header__list').toggleClass('active');
+			$('body').toggleClass('lock');
 	});
 });
