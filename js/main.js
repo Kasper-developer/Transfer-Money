0dcff4c72cc5648ee1686ae2ce319b636d1d6ec2
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-04-22 23:31:04 +0500

    Исправлен js-file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 $(document).ready(function() {
 	$('.header__burger').click(function(event){
 			$('.header__burger').toggleClass('active');
@@ -7,13 +7,3 @@ $(document).ready(function() {
 			$('body').toggleClass('lock');
 	});
 });
-=======
-$(document).ready(function() {
-	$('.header__burger').click(function(event){
-			$('.header__burger').toggleClass('active');
-			$('.header__nav').toggleClass('active');
-			$('.header__list').toggleClass('active');
-			$('body').toggleClass('lock');
-	});
-});
->>>>>>> 44be9567802847cf0c86f02387ddfb54fc8042e2
